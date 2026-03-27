badbacafbf9c2050e097001fe82084dd5b2b3a01
DESKTOP-Barni <barnikissbarni@gmail.com>
2026-03-27 15:02:14 +0100

    const res

--- HatekonyCPP/fraction.cpp
+++ HatekonyCPP/fraction.cpp
@@ -29,7 +29,7 @@ Fraction& Fraction::SimplestForm()
 {
 	if (numerator == 0)
 		return *this;
-	int res = std::gcd(numerator, denominator);
+	const int res = std::gcd(numerator, denominator);
 	if (res > 1)
 	{
 		std::cout << *this << " fraction simplified by " << res << std::endl;
