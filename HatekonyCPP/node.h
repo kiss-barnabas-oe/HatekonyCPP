5d19154cbf92311ccef0433440977b34c254c6df
DESKTOP-Barni <barnikissbarni@gmail.com>
2026-04-09 08:34:56 +0200

    Node

---
+++ HatekonyCPP/node.h
@@ -0,0 +1,56 @@
+#pragma once
+
+template<typename K, typename T>
+struct Node
+{
+	Node(const K& key, const T& value, Node* const left = nullptr, Node* const right = nullptr);
+	Node(const Node& other);
+	~Node();
+
+	Node& operator=(const Node& other);
+
+	K key;
+	T value;
+	Node* left;
+	Node* right;
+};
+
+template<typename K, typename T>
+Node<K, T>::Node(const K& key, const T& value, Node* const left, Node* const right)
+	: key(key)
+	, value(value)
+	, left(left)
+	, right(right)
+{
+}
+
+template<typename K, typename T>
+Node<K, T>::Node(const Node& other)
+	: key(other.key)
+	, value(other.value)
+	, left(other.left ? new Node(*other.left) : nullptr)
+	, right(other.right ? new Node(*other.right) : nullptr)
+{
+}
+
+template<typename K, typename T>
+Node<K, T>::~Node()
+{
+	delete left;
+	delete right;
+}
+
+template<typename K, typename T>
+Node<K, T>& Node<K, T>::operator=(const Node& other)
+{
+	if (this != &other)
+	{
+		key = other.key;
+		value = other.value;
+		delete left;
+		left = other.left ? new Node(*other.left) : nullptr;
+		delete right;
+		right = other.right ? new Node(*other.right) : nullptr;
+	}
+	return *this;
+}
